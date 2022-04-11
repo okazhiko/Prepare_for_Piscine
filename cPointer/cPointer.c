86281f05546bfc084820227df37922724628a649
okazhiko <o.kazhiko@gmail.com>
2022-04-11 23:23:53 +0900

    update

---
+++ cPointer/cPointer.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+int main(){
+    int sum = 0;
+
+    for (int i = 1; i <= 100; i++) {
+        sum += i;
+        printf("%d\n", sum);
+    }
+}
+
+int main(void){
+    printf("_Bool: %d\n", (int)sizeof(_Bool));
+    printf("char: %d\n", (int)sizeof(char));
+    printf("short: %d\n", (int)sizeof(short));
+    printf("int: %d\n", (int)sizeof(int));
+    printf("long: %d\n", (int)sizeof(long));
+    printf("long long: %d\n", (int)sizeof(long long));
+    printf("float: %d\n", (int)sizeof(float));
+    printf("double: %d\n", (int)sizeof(double));
+}
+
+
+#include <stdio.h>
+
+int main(void)
+{
+    int hoge;
+    int *hoge_p;
+
+    hoge_p = &hoge;
+
+    printf("hoge: %p\n", &hoge);
+
+    printf("hoge_p: %p\n", (void*)hoge_p);
+
+    hoge_p ++;
+
+    printf("hoge_p: %p\n", (void*)(hoge_p));
+    printf("hoge_p: %p\n", (void*)(hoge_p + 3));
+    return 0;
+}
+*/
